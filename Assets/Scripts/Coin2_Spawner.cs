1771e44c4851822cf3fd400e3dc9f4158cb0e37b
Zeinab1204 <abdi17mogahdam@gmail.com>
2023-01-19 07:09:13 -0800

    Add Coin2_Spawner

---
+++ Assets/Scripts/Coin2_Spawner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coins2_Spawnerer : MonoBehaviour
+{
+    public GameObject coin2Prefabs;
+
+    void Start()
+    {
+        StartCoroutine(Coin2Spawner());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Coin2Spawn()
+    {
+        float rand= Random.Range(-2.17f,2.17f);
+        Instantiate (coin2Prefabs,new Vector3(rand,transform.position.y,transform.position.z),Quaternion.identity);
+    }
+    IEnumerator Coin2Spawner(){
+        while(true){
+            int time =Random.Range(10,10);
+            yield return new WaitForSeconds(time);
+           Coin2Spawn();
+        }
+         
+    }
+
+}
