b482fe9d78b3e2b669a3537aca96a350196d8a03
Zeinab1204 <abdi17mogahdam@gmail.com>
2023-01-20 10:13:35 -0800

    Create pause, restart, resume buttons

--- Assets/Scripts/Game_Controler.cs
+++ Assets/Scripts/Game_Controler.cs
@@ -13,10 +13,13 @@ public class Game_Controler : MonoBehaviour
     public int highScore;
 
     public score_Manager score_manager;
+    public GameObject gamePausePanel;
+    public GameObject gamePauseButton;
 
     void Start()
     {
-        
+        gamePausePanel.SetActive(false);
+        gamePauseButton.SetActive(true);
     }
 
     // Update is called once per frame
@@ -30,5 +33,15 @@ public class Game_Controler : MonoBehaviour
     }
    public void Restart(){
         SceneManager.LoadScene("Game");
+    }
+    public void puaseGame(){
+        Time.timeScale=0;
+        gamePausePanel.SetActive(true);
+        gamePauseButton.SetActive(false);
+    }
+      public void ResumeGame(){
+        Time.timeScale=0;
+         gamePausePanel.SetActive(false);
+         gamePauseButton.SetActive(true);
     }
 }
